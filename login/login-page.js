82a6c6d6ddd7b382fa13d7331fe3147f925f269d
Vipin <100697737+vipin-surfly@users.noreply.github.com>
2023-02-08 11:45:30 +0100

    Update login-page.js

--- login/login-page.js
+++ login/login-page.js
@@ -1,19 +1,16 @@
-const loginAsync = async () =>{
-        const res = await githubLogin();
-        
-        //This is where we create the Cookie. Note the syntax.
-		//The JavaScript object we created here is just stuff for me to use later.
-        
-        Cookies.set('userInfo', {
-            username: res.additionalUserInfo.username,
-            pfp: (res.additionalUserInfo.profile.avatar_url).toString()+'.png',
-            accessToken: res.credential.accessToken
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-        }, {expires: 29})
-        
-		//The expires line basically says the cookie expires in 29 days.
-	
-		//this is not a part of js-cookie but if you're interested, I have a getter and setter with React's useState hook which basically
-		//says if it has to redirect to the main content.
-        setRedirect(true);
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
     }
+})
