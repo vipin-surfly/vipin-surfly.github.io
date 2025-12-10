f9389c4c524730cd9105883c6704b6e1366ec1ea
Vipin <100697737+vipin-surfly@users.noreply.github.com>
2025-12-10 16:02:50 +0100

    Add Surfly script initialization to script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+(function(s,u,r,f,l,y){s[f]=s[f]||{init:function(){s[f].q=arguments}};
+l=u.createElement(r);y=u.getElementsByTagName(r)[0];l.async=1;
+l.src='https://surfly.com/surfly.js';y.parentNode.insertBefore(l,y);})
+(window,document,'script','Surfly');
